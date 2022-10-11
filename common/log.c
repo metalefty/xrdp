2ec6559389a9d072139c1374bfdf5a0569a613c6
Koichiro IWAO <meta@vmeta.jp>
2022-10-11 09:03:52 +0900

    log: quit using lrint and -lm

--- common/log.c
+++ common/log.c
@@ -1158,7 +1158,7 @@ getFormattedDateTime(char *replybuf, int bufsize)
     gettimeofday(&tv, NULL);
     now = localtime(&tv.tv_sec);
 
-    millisec = lrint(tv.tv_usec / 1000.0);
+    millisec = (tv.tv_usec + 500 / 1000);
     g_snprintf(buf_millisec, sizeof(buf_millisec), "%03d", millisec);
 
     strftime(buf_datetime, sizeof(buf_datetime), "%FT%T.", now);
